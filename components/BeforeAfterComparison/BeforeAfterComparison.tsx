890312e0981392fe7da70eface0cecec3c5f58e3
Kadir Yazıcı <47540799+kadiryazici@users.noreply.github.com>
2026-09-08 17:24:38 +0300

    fix: background color of photo card

--- components/BeforeAfterComparison/BeforeAfterComparison.tsx
+++ components/BeforeAfterComparison/BeforeAfterComparison.tsx
@@ -66,7 +66,7 @@ function PhotoCard(props: PhotoCardProps) {
       {...attrs}
       className={cn("border-[1.02px] border-transparent p-[12.21px] rounded-xl w-[462.15px] h-[524.59px] shrink-0 flex", className)}
     >
-      <div className="overflow-clip isolate relative rounded-lg bg-primary-400 min-w-0 size-full">
+      <div className="overflow-clip isolate relative rounded-lg bg-bg-secondary-hover min-w-0 size-full">
         <h3 className="w-full py-[16.15px] uppercase font-zagma text-body-3-zagma text-center text-text-button-primary">{heading}</h3>
 
         <div className="absolute z-[-1] size-full top-[26.91px] left-0 right-0 *:size-full *:object-cover">
